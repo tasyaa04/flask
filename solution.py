32998ba99f5a25556924aa181fb4deeee0427ce3
tasyaa04 <anokhita4@gmail.com>
2021-03-23 21:05:59 +0300

    Revert "mars"
    This reverts commit 1fd47028

--- solution.py
+++
@@ -1,7 +0,0 @@
-name = input()
-global_init(name)
-db_sess = create_session()
-
-for user in db_sess.query(User).filter(User.address == "module_1", User.position.notilike("%engineer%"),
-                                       User.speciality.notilike("%engineer%")):
-    print(user.id)
